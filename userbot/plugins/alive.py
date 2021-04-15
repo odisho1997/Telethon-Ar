9d6967cb752fd83f10bc232fff24d015c7d54ac1
i.q <64720410+klanrali@users.noreply.github.com>
2021-04-16 01:06:07 +0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -5,10 +5,10 @@ from telethon import version
 
 from . import ALIVE_NAME, StartTime, catversion, get_readable_time, mention, reply_id
 
-DEFAULTUSER = ALIVE_NAME or "Telethon-Arabs"
-CAT_IMG = Config.ALIVE_PIC
-CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or "𝑤𝑒𝑙𝑐𝑜𝑚𝑒 𝑡𝑜 𝑠𝑜𝑢𝑟𝑐𝑒 𝑡𝑒𝑙𝑒𝑡ℎ𝑜𝑛 𝑎𝑟𝑎𝑏𝑠⁦ ˖꒰"
-EMOJI = Config.CUSTOM_ALIVE_EMOJI or "  𖠕 "
+DEFAULTUSER = ALIVE_NAME or "I𝐐𝐓𝐇𝐎𝐍⁦♡⁩"
+CAT_IMG = Config.ALIVE_PIC or "https://telegra.ph/file/583477aba4b6b294750c3.jpg"
+CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or "𓆩 𝙬𝙚𝙡𝙘𝙤𝙢𝙚 𝙩𝙤 𝙩𝙚𝙡𝙚𝙩𝙝𝙤𝙣 𝙖𝙧𝙖𝙗𝙨 𝆹𝅥𝅮"
+EMOJI = Config.CUSTOM_ALIVE_EMOJI or "𖠕"
 
 
 @bot.on(admin_cmd(outgoing=True, pattern="alive$"))
@@ -20,15 +20,15 @@ async def amireallyalive(alive):
     uptime = await get_readable_time((time.time() - StartTime))
     _, check_sgnirts = check_data_base_heal_th()
     if CAT_IMG:
-        cat_caption = f"**{CUSTOM_ALIVE_TEXT}**\n\n"
+        cat_caption = f"**{CUSTOM_ALIVE_TEXT}**\n"
         cat_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧᵗᵉˡᵉᵗʰᵒᶰ ᵃʳᵃᵇˢ⁦⁦ⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
-        cat_caption += f"**{EMOJI} قاعدة البيانات :** `{check_sgnirts}`\n"
-        cat_caption += f"**{EMOJI} نسخة تليثون :** `{version.__version__}\n`"
-        cat_caption += f"**{EMOJI} نسخة العرب :** `{catversion}`\n"
-        cat_caption += f"**{EMOJI} نسخة البايثون :** `{python_version()}\n`"
-        cat_caption += f"**{EMOJI} الوقت :** `{uptime}\n`"
-        cat_caption += f"**{EMOJI} المنشئ:** {mention}\n"
-        cat_caption += f"**{EMOJI}**  **[قناة السورس]**(t.me/iqthon)  𖠕 .\n"
+        cat_caption += f"**{EMOJI} قاعدة البيانات 𝆹𝅥𝅮** `{check_sgnirts}`\n"
+        cat_caption += f"**{EMOJI} اصدار التليثون  𝆹𝅥𝅮** `{version.__version__}\n`"
+        cat_caption += f"**{EMOJI} اصدار تليثون العرب 𝆹𝅥𝅮** `{catversion}`\n"
+        cat_caption += f"**{EMOJI} اصدار البايثون 𝆹𝅥𝅮** `{python_version()}\n`"
+        #        cat_caption += f"**{EMOJI} مدة التشغيل 𝆹𝅥𝅮** `{uptime}\n`"
+        cat_caption += f"**{EMOJI} المستخدم 𝆹𝅥𝅮** {mention}\n"
+        cat_caption += f"**{EMOJI} مطور السورس 𝆹𝅥𝅮** [اضغط هنا](t.me/iqthon) 𓆰.\n"
         cat_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧᵗᵉˡᵉᵗʰᵒᶰ ᵃʳᵃᵇˢ⁦⁦ⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
         await alive.client.send_file(
             alive.chat_id, CAT_IMG, caption=cat_caption, reply_to=reply_to_id
@@ -38,47 +38,19 @@ async def amireallyalive(alive):
         await edit_or_reply(
             alive,
             f"**{CUSTOM_ALIVE_TEXT}**\n\n"
-            f"**{EMOJI} قاعدة البيانات :** `{check_sgnirts}`\n"
-            f"**{EMOJI} نسخة تليثون :** `{version.__version__}\n`"
-            f"**{EMOJI} نسخة العرب :** `{catversion}`\n"
-            f"**{EMOJI} نسخة البايثون :** `{python_version()}\n`"
-            f"**{EMOJI} الوقت :** `{uptime}\n`"
-            f"**{EMOJI} المنشئ:** {mention}\n",
+            f"**{EMOJI} قاعدة البيانات 𝆹𝅥𝅮**  `{check_sgnirts}`\n"
+            f"**{EMOJI} اصدار التليثون  𝆹𝅥𝅮** `{version.__version__}\n`"
+            f"**{EMOJI} اصدار تليثون العرب 𝆹𝅥𝅮** `{catversion}`\n"
+            f"**{EMOJI} اصدار البايثون  𝆹𝅥𝅮** `{python_version()}\n`"
+            f"**{EMOJI} مدة التشغيل 𝆹𝅥𝅮** `{uptime}\n`"
+            f"**{EMOJI} المستخدم 𝆹𝅥𝅮** {mention}\n",
         )
-
-
-@bot.on(admin_cmd(outgoing=True, pattern="idkbsalive$"))
-@bot.on(sudo_cmd(pattern="ialive$", allow_sudo=True))
-async def amireallyalive(alive):
-    if alive.fwd_from:
-        return
-    tgbotusername = Config.TG_BOT_USERNAME
-    reply_to_id = await reply_id(alive)
-    cat_caption = f"**Catuserbot is Up and Running**\n"
-    cat_caption += f"**  -Telethon version :** `{version.__version__}\n`"
-    cat_caption += f"**  -Catuserbot Version :** `{catversion}`\n"
-    cat_caption += f"**  -Python Version :** `{python_version()}\n`"
-    cat_caption += f"**  -Master:** {mention}\n"
-    results = await bot.inline_query(tgbotusername, cat_caption)  # pylint:disable=E0602
-    await results[0].click(alive.chat_id, reply_to=reply_to_id, hide_via=True)
-    await alive.delete()
-
-
-# UniBorg Telegram UseRBot
-# Copyright (C) 2020 @UniBorg
-# This code is licensed under
-# the "you can't use this for anything - public or private,
-# unless you know the two prime factors to the number below" license
-# 543935563961418342898620676239017231876605452284544942043082635399903451854594062955
-# വിവരണം അടിച്ചുമാറ്റിക്കൊണ്ട് പോകുന്നവർ
-# ക്രെഡിറ്റ് വെച്ചാൽ സന്തോഷമേ ഉള്ളു..!
-# uniborg
 
 
 def check_data_base_heal_th():
     # https://stackoverflow.com/a/41961968
     is_database_working = False
-    output = "No Database is set"
+    output = "لم يتم تعيين قاعدة بيانات"
     if not Config.DB_URI:
         return is_database_working, output
     from userbot.plugins.sql_helper import SESSION
@@ -90,7 +62,7 @@ def check_data_base_heal_th():
         output = f"❌ {str(e)}"
         is_database_working = False
     else:
-        output = "Functioning Normally"
+        output = "تعمل بنجاح"
         is_database_working = True
     return is_database_working, output
 

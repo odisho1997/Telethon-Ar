77c8770dc001ddb10d4f4ade50c59122fad622e1
i.q <64720410+klanrali@users.noreply.github.com>
2021-04-22 13:49:06 +0300

    Update and rename gali.py to s18+_AR.py

--- userbot/plugins/s18+_AR.py
+++ userbot/plugins/s18+_AR.py
@@ -3,49 +3,6 @@ import random
 
 from . import catmemes
 
-
-@bot.on(admin_cmd(outgoing=True, pattern="abuse$"))
-@bot.on(sudo_cmd(pattern="abuse$", allow_sudo=True))
-async def abusing(abused):
-    reply_text = random.choice(catmemes.ABUSE_STRINGS)
-    await edit_or_reply(abused, reply_text)
-
-
-@bot.on(admin_cmd(outgoing=True, pattern="abusehard$"))
-@bot.on(sudo_cmd(pattern="abusehard$", allow_sudo=True))
-async def fuckedd(abusehard):
-    reply_text = random.choice(catmemes.ABUSEHARD_STRING)
-    await edit_or_reply(abusehard, reply_text)
-
-
-@bot.on(admin_cmd(outgoing=True, pattern="rendi$"))
-@bot.on(sudo_cmd(pattern="rendi$", allow_sudo=True))
-async def metoo(e):
-    txt = random.choice(catmemes.RENDISTR)
-    await edit_or_reply(e, txt)
-
-
-@bot.on(admin_cmd(outgoing=True, pattern="rape$"))
-@bot.on(sudo_cmd(pattern="rape$", allow_sudo=True))
-async def raping(raped):
-    reply_text = random.choice(catmemes.RAPE_STRINGS)
-    await edit_or_reply(raped, reply_text)
-
-
-@bot.on(admin_cmd(outgoing=True, pattern="fuck$"))
-@bot.on(sudo_cmd(pattern="fuck$", allow_sudo=True))
-async def chutiya(fuks):
-    reply_text = random.choice(catmemes.CHU_STRINGS)
-    await edit_or_reply(fuks, reply_text)
-
-
-@bot.on(admin_cmd(outgoing=True, pattern="thanos$"))
-@bot.on(sudo_cmd(pattern="thanos$", allow_sudo=True))
-async def thanos(thanos):
-    reply_text = random.choice(catmemes.THANOS_STRINGS)
-    await edit_or_reply(thanos, reply_text)
-
-
 @bot.on(admin_cmd(outgoing=True, pattern="kiss$"))
 @bot.on(sudo_cmd(pattern="kiss$", allow_sudo=True))
 async def _(event):

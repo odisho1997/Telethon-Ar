7046ab3b6969b49244dc97c01756e6316a6a5103
i.q <64720410+klanrali@users.noreply.github.com>
2021-04-23 15:25:37 +0300

    Update and rename mention.py to mention_AR.py

--- userbot/plugins/mention_AR.py
+++ userbot/plugins/mention_AR.py
@@ -9,9 +9,9 @@ async def _(event):
     reply_to_id = event.message
     if event.reply_to_msg_id:
         reply_to_id = await event.get_reply_message()
-    mentions = "@all"
+    mentions = "تـم عمـل تـاك لـ 200 شـخص"
     chat = await event.get_input_chat()
-    async for x in event.client.iter_participants(chat, 100):
+    async for x in event.client.iter_participants(chat, 200):
         mentions += f"[\u2063](tg://user?id={x.id})"
     await reply_to_id.reply(mentions)
     await event.delete()
@@ -24,9 +24,9 @@ async def _(event):
         return
     reply_to_id = await reply_id(event)
     input_str = event.pattern_match.group(1)
-    mentions = input_str or "@all"
+    mentions = input_str or "تـم عمـل تـاك لـ 200 شـخص"
     chat = await event.get_input_chat()
-    async for x in event.client.iter_participants(chat, 100):
+    async for x in event.client.iter_participants(chat, 200):
         mentions += f"[\u2063](tg://user?id={x.id})"
     await event.client.send_message(event.chat_id, mentions, reply_to=reply_to_id)
     await event.delete()
@@ -37,7 +37,7 @@ async def _(event):
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "@admin: **Spam Spotted**"
+    mentions = "@admin: **تـم عـمل ابـلاغ للأدمنـية**"
     chat = await event.get_input_chat()
     reply_to_id = await reply_id(event)
     async for x in event.client.iter_participants(
@@ -48,38 +48,6 @@ async def _(event):
     await event.client.send_message(event.chat_id, mentions, reply_to=reply_to_id)
     await event.delete()
 
-
-@bot.on(admin_cmd(pattern="men (.*)"))
-@bot.on(sudo_cmd(pattern="men (.*)", allow_sudo=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    input_str = event.pattern_match.group(1)
-    reply_to_id = await reply_id(event)
-    if event.reply_to_msg_id:
-        reply_msg = await event.get_reply_message()
-        u = reply_msg.sender_id
-    else:
-        user, input_str = input_str.split(" ", 1)
-        try:
-            u = int(user)
-        except ValueError:
-            try:
-                u = await event.client.get_entity(user)
-            except ValueError:
-                await event.delete()
-                return
-            u = int(u.id)
-        except Exception:
-            await event.delete()
-            return
-    await event.delete()
-    await event.client.send_message(
-        event.chat_id,
-        f"<a href='tg://user?id={u}'>{input_str}</a>",
-        parse_mode="HTML",
-        reply_to=reply_to_id,
-    )
 
 
 CMD_HELP.update(

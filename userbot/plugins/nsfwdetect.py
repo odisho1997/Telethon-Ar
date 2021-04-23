2db54d2bb2dc8037343e966872b9bee77bde1049
i.q <64720410+klanrali@users.noreply.github.com>
2021-04-23 19:31:38 +0300

    Update nsfwdetect.py

--- userbot/plugins/nsfwdetect.py
+++ userbot/plugins/nsfwdetect.py
@@ -11,12 +11,12 @@ import requests
 async def detect(event):
     if Config.DEEP_AI is None:
         return await edit_delete(
-            event, "Add VAR `DEEP_AI` get Api Key from https://deepai.org/", 5
+            event, "أضـف VAR `DEEP_AI` اجـلب المـفتاح من هـنا from https://deepai.org/", 5
         )
     reply = await event.get_reply_message()
     if not reply:
         return await edit_delete(
-            event, "`Reply to any image or non animated sticker !`", 5
+            event, "`قـم برد على المـلصق !`", 5
         )
     catevent = await edit_or_reply(event, "`Downloading the file to check...`")
     media = await event.client.download_media(reply)
